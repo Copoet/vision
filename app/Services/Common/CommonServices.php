7e08f4f4ba8e73242bfe920d029293bbfb566c41
copoet <copoet@126.com>
2020-08-11 14:38:41 +0800

    vendor update

---
+++ app/Services/Common/CommonServices.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/11/2:15 PM
+ */
+
+namespace App\Services\Common;
+
+
+class CommonServices
+{
+
+}
---
+++ app/Services/Common/ValidateServices.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/11/2:27 PM
+ */
+
+namespace App\Services\Common;
+
+
+class ValidateServices
+{
+
+}
