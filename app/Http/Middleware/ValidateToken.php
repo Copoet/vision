1abfd8419e7497fd55f1af6acf5eb4a863174760
copoet <copoet@126.com>
2020-08-12 23:16:30 +0800

    middleware

---
+++ app/Http/Middleware/ValidateToken.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Model\ManagerModel;
+use Closure;
+
+class CheckToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->input('token');
+        if (empty($token)) {
+            $result['code'] = '4001';
+            $result['status'] = false;
+            $result['msg'] = '操作参数不能为空';
+            return Response()->json($result);
+        }
+        $checkResult = ManagerModel::checkManagerToken($token);
+        if (empty($checkResult)) {
+            $result['code'] = '4001';
+            $result['status'] = false;
+            $result['msg'] = 'token不合法';
+            return Response()->json($result);
+        }
+//        if ((time() - 86400) > $checkResult->last_time) {
+//            $result['code'] = '4001';
+//            $result['status'] = false;
+//            $result['msg'] = '会话过期,请重新登录';
+//            return Response()->json($result);
+//        }
+        return $next($request);
+    }
+
+}
