a21cadb87fe71bc311be3a4bea7470067bf63251
copoet <copoet@126.com>
2020-08-17 17:33:30 +0800

    add controller

---
+++ app/Services/MenuService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/17/5:12 PM
+ */
+
+namespace App\Services;
+
+
+class MenuService
+{
+
+}
---
+++ app/Services/SystemService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/17/5:13 PM
+ */
+
+namespace App\Services;
+
+
+class SystemService
+{
+
+}
