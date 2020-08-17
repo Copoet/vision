918b654b864cb9ed204bac6bee8d4d2ad81965a1
copoet <copoet@126.com>
2020-08-17 17:37:25 +0800

    add controller

---
+++ app/Http/Controllers/Admin/SystemController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/17/5:35 PM
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class SystemController
+{
+
+}
