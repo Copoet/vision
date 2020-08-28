c2c756985004899575a2c8aaa004c9b9995590fa
copoet <copoet@126.com>
2020-08-28 17:50:01 +0800

    stock init

---
+++ app/Http/Controllers/Admin/StockController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/28/5:48 PM
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class StockController extends Controller
+{
+
+
+}
---
+++ app/Services/StockService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: copoet
+ * Mail: copoet@126.com
+ * Date: 2020/8/28/5:49 PM
+ */
+
+namespace App\Services;
+
+
+class StockService
+{
+
+}
