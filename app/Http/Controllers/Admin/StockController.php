c363ddb938872215896841265cc07abd7b44a342
copoet <copoet@126.com>
2020-08-28 19:19:55 +0800

    stock list

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -10,9 +10,43 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Services\Common\CodeService;
+use App\Services\StockService;
+use Illuminate\Http\Request;
 
 class StockController extends Controller
 {
 
+    protected $stockService;
 
+
+    public function __construct(StockService $service)
+    {
+        $this->stockService = $service;
+    }
+
+
+    /**
+     * @param Request $request
+     * @author copoet
+     * @mail copoet@126.com
+     * Date: 2020/8/28/7:18 PM
+     */
+    public function stockList(Request $request)
+    {
+        $page     = $request->input('page') ? $request->input('page') : 1;
+        $pageSize = $request->input('page_size') ? $request->input('page_size') : 20;
+        $param    = $request->all();
+
+        $list = $this->stockService->getStockList($param, $page, $pageSize);
+
+        if ($list) {
+
+            $this->returnSuccess($list, CodeService::PUBLIC_SUCCESS);
+
+        } else {
+            $this->returnFail(CodeService::PUBLIC_ERROR);
+        }
+
+    }
 }
--- app/Services/StockService.php
+++ app/Services/StockService.php
@@ -9,7 +9,46 @@
 namespace App\Services;
 
 
+use App\Models\Stock;
+
 class StockService
 {
 
+
+    /**
+     * 列表
+     * @param $where
+     * @param array $columns
+     * @param $page
+     * @param $pageSize
+     * @return mixed
+     * @author copoet
+     * @mail copoet@126.com
+     * Date: 2020/8/28/7:16 PM
+     */
+    public function getStockList($where, $columns = ['*'], $page, $pageSize)
+    {
+
+        $result['total'] = Stock::query()
+            ->where(function ($query) use ($where) {
+                if (isset($param['keyWords'])) {
+                    $query->where('stock_name', 'like', '%' . $where['keyWords'] . '%');
+                }
+            })->count();
+
+        $offset = ($page - 1) * $pageSize;
+
+        $result['list'] = Stock::query()->where(function ($query) use ($where) {
+            if (isset($param['keyWords'])) {
+                $query->where('stock_name', 'like', '%' . $where['keyWords'] . '%');
+            }
+        })
+            ->offset($offset)
+            ->limit($pageSize)
+            ->get($columns)
+            ->toArray();
+
+        return $result;
+
+    }
 }
