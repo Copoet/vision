ce9e02c9554bff0c70a5c568d6946ec7a2eeb69a
copoet <copoet@126.com>
2020-10-21 17:20:38 +0800

    model base

--- app/Models/ArticleSort.php
+++ app/Models/ArticleSort.php
@@ -6,7 +6,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Enum\ArticleEnum;
 class ArticleSort extends Model
 {
+    protected $table = 'article_sort';
 
+
+    protected $primaryKey = 'id';
+
+
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+
+    public function getStatusAttribute($value)
+    {
+
+        return ArticleEnum::STATUS[$value];
+    }
+
+
+    public function getIsDeleteAttribute($value)
+    {
+        return ArticleEnum::DELETE[$value];
+    }
+
+    public function getCreateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+
+    public function getUpdateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
 }
