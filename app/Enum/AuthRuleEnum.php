a7a5c98d5d3705462e50df6e7fad988c60ba703c
copoet <copoet@126.com>
2020-11-20 17:34:11 +0800

    auth enum

---
+++ app/Enum/AuthRuleEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enum;
+
+
+class AuthRuleEnum
+{
+    const STATUS = [
+        1 => '启用',
+        2 => '禁用'
+    ];
+
+    const DELETE =[
+        1 => '是',
+        2 => '否'
+    ];
+}
