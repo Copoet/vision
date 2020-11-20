6354f073ee291bc9f5fa9d510cf7a4610c825294
copoet <copoet@126.com>
2020-11-20 18:49:40 +0800

    model base

--- app/Models/AuthRoleRule.php
+++ app/Models/AuthRoleRule.php
@@ -1,12 +1,43 @@
 <?php
 
-
 namespace App\Models;
 
 
+use App\Enum\AuthRoleRuleEnum;
 use Illuminate\Database\Eloquent\Model;
 
 class AuthRoleRule extends Model
 {
+    protected $table = 'auth_role_rule';
 
+
+    protected $primaryKey = 'id';
+
+
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+
+    public function getStatusAttribute($value)
+    {
+        return AuthRoleRuleEnum::STATUS[$value];
+    }
+
+
+    public function getIsDeleteAttribute($value)
+    {
+        return AuthRoleRuleEnum::DELETE[$value];
+    }
+
+    public function getCreateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+
+    public function getUpdateTimeAttribute($value)
+    {
+
+        return date('Y-m-d H:i:s', strtotime($value));
+    }
 }
--- app/Models/AuthRule.php
+++ app/Models/AuthRule.php
@@ -4,8 +4,41 @@
 namespace App\Models;
 
 
+use App\Enum\AuthRuleEnum;
 use Illuminate\Database\Eloquent\Model;
 
 class AuthRule extends Model {
 
+    protected $table = 'auth_rule';
+
+
+    protected $primaryKey = 'id';
+
+
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+
+    public function getStatusAttribute($value)
+    {
+        return AuthRuleEnum::STATUS[$value];
+    }
+
+
+    public function getIsDeleteAttribute($value)
+    {
+        return AuthRuleEnum::DELETE[$value];
+    }
+
+    public function getCreateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+
+    public function getUpdateTimeAttribute($value)
+    {
+
+        return date('Y-m-d H:i:s', strtotime($value));
+    }
 }
