72ebad96be28dd2facb21b37441e0784c16e7bcd
copoet <copoet@126.com>
2020-11-23 11:23:04 +0800

    add auth controller

--- app/Http/Controllers/Admin/AuthManagerGroupController.php
+++ app/Http/Controllers/Admin/AuthManagerGroupController.php
@@ -5,8 +5,69 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Services\AuthManagerGroupService;
+use App\Services\Common\CodeService;
+use Illuminate\Http\Request;
 
 class AuthManagerGroupController extends Controller
 {
+    protected $authManagerGroup;
 
+
+    public function __construct(AuthManagerGroupService $service)
+    {
+        $this->authManagerGroup= $service;
+    }
+
+
+    /**
+     * 获取列表
+     * @param Request $request
+     */
+    public function list(Request $request)
+    {
+        $page     = $request->input('page') ? $request->input('page') : 1;
+        $pageSize = $request->input('page_size') ? $request->input('page_size') : 20;
+        $param    = $request->all();
+
+        $list = $this->authManagerGroup->getList($param, $page, $pageSize);
+
+        if ($list) {
+            $this->returnSuccess($list);
+        } else {
+            $this->returnFail(CodeService::PUBLIC_ERROR);
+        }
+
+    }
+
+
+    /**
+     * 添加操作
+     * @param Request $request
+     */
+    public function create(Request $request){
+
+
+
+    }
+
+
+    /**
+     * 更新操作
+     * @param Request $request
+     */
+    public function update(Request $request){
+
+    }
+
+
+    /**
+     * 删除操作
+     * @param Request $request
+     */
+    public function delete(Request $request){
+
+
+
+    }
 }
--- app/Http/Controllers/Admin/AuthRoleRuleController.php
+++ app/Http/Controllers/Admin/AuthRoleRuleController.php
@@ -4,8 +4,69 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Services\AuthRoleRuleService;
+use App\Services\Common\CodeService;
+use Illuminate\Http\Request;
 
 class AuthRoleRuleController extends Controller
 {
+    protected $authRoleRuleService;
 
+
+    public function __construct(AuthRoleRuleService $service)
+    {
+        $this->authRoleRuleService= $service;
+    }
+
+
+    /**
+     * 获取列表
+     * @param Request $request
+     */
+    public function list(Request $request)
+    {
+        $page     = $request->input('page') ? $request->input('page') : 1;
+        $pageSize = $request->input('page_size') ? $request->input('page_size') : 20;
+        $param    = $request->all();
+
+        $list = $this->authRoleRuleService->getList($param, $page, $pageSize);
+
+        if ($list) {
+            $this->returnSuccess($list);
+        } else {
+            $this->returnFail(CodeService::PUBLIC_ERROR);
+        }
+
+    }
+
+
+    /**
+     * 添加操作
+     * @param Request $request
+     */
+    public function create(Request $request){
+
+
+
+    }
+
+
+    /**
+     * 更新操作
+     * @param Request $request
+     */
+    public function update(Request $request){
+
+    }
+
+
+    /**
+     * 删除操作
+     * @param Request $request
+     */
+    public function delete(Request $request){
+
+
+
+    }
 }
