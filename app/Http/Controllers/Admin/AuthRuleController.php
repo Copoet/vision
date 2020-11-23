95e6d0d114e15648faf92088afb031a328ee5a37
copoet <copoet@126.com>
2020-11-23 14:39:11 +0800

    add auth rule action

--- app/Http/Controllers/Admin/AuthRuleController.php
+++ app/Http/Controllers/Admin/AuthRuleController.php
@@ -47,8 +47,31 @@ class AuthRuleController extends Controller
      * 添加操作
      * @param Request $request
      */
-    public function create(Request $request){
+    public function create(Request $request)
+    {
 
+        $menuId = $request->input('menu_id');
+        $action = $request->input('action');
+        $name   = $request->input('name');
+        $status = $request->input('status');
+
+        if (empty($name) || empty($menuId) || empty($action)) {
+
+            $this->returnFail(CodeService::PUBLIC_PARAMS_NULL);
+        }
+
+        $data['menu_id'] = $menuId;
+        $data['name']    = $name;
+        $data['status']  = $status;
+        $data['action']  = $action;
+
+        $result = $this->authRuleService->store($data);
+
+        if ($result) {
+            $this->returnSuccess($result);
+        } else {
+            $this->returnFail(CodeService::PUBLIC_ERROR);
+        }
 
 
     }
@@ -58,8 +81,33 @@ class AuthRuleController extends Controller
      * 更新操作
      * @param Request $request
      */
-    public function update(Request $request){
+    public function update(Request $request)
+    {
+        $menuId   = $request->input('menu_id');
+        $action   = $request->input('action');
+        $name     = $request->input('name');
+        $status   = $request->input('status');
+        $id       = $request->input('id');
+        $isDelete = $request->input('is_delete');
 
+        if (empty($name) || empty($menuId) || empty($action) || empty($id)) {
+
+            $this->returnFail(CodeService::PUBLIC_PARAMS_NULL);
+        }
+
+        $data['menu_id']   = $menuId;
+        $data['name']      = $name;
+        $data['status']    = $status;
+        $data['action']    = $action;
+        $data['is_delete'] = $isDelete;
+
+        $result = $this->authRuleService->update(['id' => $id], $data);
+
+        if ($result) {
+            $this->returnSuccess($result);
+        } else {
+            $this->returnFail(CodeService::PUBLIC_ERROR);
+        }
     }
 
 
@@ -67,9 +115,22 @@ class AuthRuleController extends Controller
      * 删除操作
      * @param Request $request
      */
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
+        $id = $request->input('id');
 
+        if (empty($id)) {
 
+            $this->returnFail(CodeService::PUBLIC_PARAMS_NULL);
+        }
+
+        $result = $this->authRuleService->del(['id' => $id]);
+
+        if ($result) {
+            $this->returnSuccess($result);
+        } else {
+            $this->returnFail(CodeService::PUBLIC_ERROR);
+        }
 
     }
 }
