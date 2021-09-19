5acb2f627633de90693604629e7c52a09a49b886
copoet <copoet@126.com>
2021-09-19 19:21:43 +0800

    add point

---
+++ app/Models/AdvertisingPoint.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Enum\CommonEnum;
+use Illuminate\Database\Eloquent\Model;
+
+
+class AdvertisingPoint extends Model
+{
+
+    protected $table = 'advertising_point';
+
+
+    protected $primaryKey = 'id';
+
+
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+
+    public function getStatusStrAttribute($value)
+    {
+
+        return CommonEnum::STATUS[$value];
+    }
+
+
+    public function getIsDeleteStrAttribute($value)
+    {
+        return CommonEnum::DELETE[$value];
+    }
+
+    public function getCreateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+
+    public function getUpdateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+}
---
+++ app/Models/PointType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Enum\CommonEnum;
+use Illuminate\Database\Eloquent\Model;
+
+
+class PointType extends Model
+{
+
+    protected $table = 'merchant';
+
+
+    protected $primaryKey = 'id';
+
+
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+
+    public function getStatusStrAttribute($value)
+    {
+
+        return CommonEnum::STATUS[$value];
+    }
+
+
+    public function getIsDeleteStrAttribute($value)
+    {
+        return CommonEnum::DELETE[$value];
+    }
+
+    public function getCreateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+
+    public function getUpdateTimeAttribute($value){
+
+        return date('Y-m-d H:i:s',strtotime($value));
+    }
+}
